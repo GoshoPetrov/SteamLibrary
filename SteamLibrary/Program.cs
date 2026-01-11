3030c61c5cde323217d19c790ca814bd7bde2888
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2026-01-11 16:06:05 +0200

    Add project files.

---
+++ SteamLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SteamLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
